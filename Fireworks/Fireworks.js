c0e8a4b79e3eafad03074bc7e025817cc83c6769
Peter Gagliardi <ptrgags@gmail.com>
2019-09-02 10:08:52 -0400

    Create an envelope; scale to fit the window

--- Fireworks/Fireworks.js
+++ Fireworks/Fireworks.js
@@ -4,6 +4,92 @@ const EXPLODE_TIME = 1;
 const FPS = 30;
 const NUM_FRAGMENTS = 1000;
 
+const v0 = 20.0;
+const g = 9.81;
+const y0= 100.0;
+
+/**
+ * Parabola that serves as the envelope of the superposition
+ * of all possible trajectories
+ */
+class Envelope {
+    static get MAX_X() {
+        const term1 = 2 * y0 * v0 * v0 / g;
+        const term2 = v0 * v0 * v0 * v0 / g / g;
+        return Math.sqrt(term1 + term2);
+    }
+
+    static get RESOLUTION() {
+        return 100;
+    }
+
+    static height(x) {
+        const term1 = y0;
+        const term2 = v0 * v0 / 2.0 / g;
+        const term3 = -g * x * x / 2.0 / v0 / v0;
+        return term1 + term2 + term3
+    }
+
+    static make_points() {
+        const points = [];
+        const xf = Envelope.MAX_X;
+        for (let i = 0; i < Envelope.RESOLUTION; i++) {
+            const t = i / (Envelope.RESOLUTION - 1);
+            const x = lerp(-xf, xf, t);
+            const y = Envelope.height(x);
+            const point = createVector(x, y);
+            points.push(point);
+        }
+        return points;
+    }
+
+    constructor() {
+        this.points = Envelope.make_points();
+    }
+
+    render() {
+        noFill();
+        stroke(255);
+        beginShape(LINES);
+        for (let i = 0; i < this.points.length - 1; i++) {
+            const prev = this.points[i];
+            const curr = this.points[i + 1];
+            vertex(prev.x, prev.y);
+            vertex(curr.x, curr.y);
+        }
+        endShape();
+
+        rect(-Envelope.MAX_X, 0, 2 * Envelope.MAX_X, 2 * Envelope.MAX_X);
+    }
+}
+
+class Scaler {
+    constructor() {
+        this.size_meters = 2.0 * Envelope.MAX_X;
+    }
+
+    get size_pixels() {
+        return min(width, height);
+    }
+
+    get meters_per_pixel() {
+        return this.size_meters / this.size_pixels;
+    }
+    
+    start_scaling() {
+        const scale_factor = 1.0 / this.meters_per_pixel;
+        push();
+        translate(width / 2, height / 2 + this.size_pixels / 2);
+        scale(1, -1);
+        scale(scale_factor, scale_factor);
+        strokeWeight(1.0 / scale_factor);
+    }
+
+    stop_scaling() {
+        pop();
+    }
+}
+
 class Rocket {
   constructor(explode_time, explode_height) {
     this.explode_time = explode_time;
@@ -82,22 +168,34 @@ class Fragment {
 }
 
 
-var rocket;
-var fragments = [];
+let rocket;
+let fragments = []; 
+let envelope;
+let scaler;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  frameRate(FPS);
-  
+    createCanvas(windowWidth, windowHeight);
+    frameRate(FPS);
+
+    envelope = new Envelope();
+    scaler = new Scaler();
+  /* 
   rocket = new Rocket(EXPLODE_TIME, EXPLODE_HEIGHT);
   for (let i = 0; i < NUM_FRAGMENTS; i++) {
     const frag = new Fragment(EXPLODE_TIME, EXPLODE_HEIGHT, SPEED);
     fragments.push(frag);
   }
+  */
 }
 
 
 function draw() {
+    background(0);
+
+    scaler.start_scaling();
+    envelope.render();
+    scaler.stop_scaling();
+    /*
   const t = frameCount / FPS;
   if (t < EXPLODE_TIME + 0.01) {
     background(0);
@@ -108,5 +206,10 @@ function draw() {
   for (let frag of fragments) {
     frag.render(t);
   }
+  */
 
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
